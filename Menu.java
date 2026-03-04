06e3dfe9786b1bcdac4e5753970048b59571ddb5
josevang0811 <josevang0811@gmail.com>
2026-03-04 16:14:40 +0000

    Menu de el e-commer

---
+++ Menu.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Menu{
+    public static void main (String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int opcion=0;
+
+        while (opcion!=5) {
+            System.out.println("===== SISTEMA E-COMMERCE =====");
+            System.out.println("1. Ver productos ordenados por precio");
+            System.out.println("2. Agregar un nuevo cliente");
+            System.out.println("3. Calcular total de ventas por producto");
+            System.out.println("4. Ver clientes que han realizado compras");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    //Jose
+                    break;
+                case 2:
+                    //Jairo
+                    break;
+                case 3:
+                    //Jesus
+                    break;
+                case 4:
+                    //Daniela
+                    break;
+                case 5:
+                    //  Quin sea
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
